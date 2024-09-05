29c0aa7051736fdf25ba8677c1b358c6254f483b
burindesroziers-julie-emf <julie.dosreis@studentfr.ch>
2024-09-05 11:44:35 +0200

    exe3 corrigé, exe7, devoir 1

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,18 @@
+public class devoir01 {
+    public static void main(String[] args) {
+        int monAge=29;
+        boolean estMajeur;
+        if (monAge>20) {
+            estMajeur=true;
+        } else {
+            estMajeur=false;
+        }
+        System.out.println("Je m'appelle BdR Julie.");
+        System.out.println("Mon âge est de : "+monAge+" ans");
+        if (estMajeur==true) {
+            System.out.println("Je suis majeur");
+        } else {
+            
+        }
+    }
+}
--- exercices/exercice05/ExerciceCondition3.java
+++ exercices/exercice05/ExerciceCondition3.java
@@ -5,7 +5,7 @@ public class ExerciceCondition3 {
         int valeur1=(int)(Math.random()*(max-min+1))+min;
         int valeur2=(int)(Math.random()*(max-min+1))+min;
         System.out.println("Les nombres à multiplier sont "+valeur1+" et "+valeur2+".");
-        if (valeur1*valeur2>=0) {
+        if (valeur1>=0 && valeur2>=0 || valeur1<0 && valeur2<0) {
             System.out.println("Le résultat est positif.");
         } else {
             System.out.println("Le résultat est négatif.");
