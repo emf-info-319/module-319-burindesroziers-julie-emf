0025e6f3511b86e557a703347e4a4fd9e7198451
burindesroziers-julie-emf <julie.dosreis@studentfr.ch>
2024-09-07 11:32:20 +0200

    devoir01 terminé

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -1,18 +1,32 @@
 public class devoir01 {
     public static void main(String[] args) {
-        int monAge=29;
+        int monAge = 29;
         boolean estMajeur;
-        if (monAge>20) {
-            estMajeur=true;
+        if (monAge >= 20) {
+            estMajeur = true;
         } else {
-            estMajeur=false;
+            estMajeur = false;
         }
         System.out.println("Je m'appelle BdR Julie.");
-        System.out.println("Mon âge est de : "+monAge+" ans");
-        if (estMajeur==true) {
-            System.out.println("Je suis majeur");
+        System.out.println("Mon âge est de : " + monAge + " ans");
+        if (estMajeur == true) { //pléonasme
+            System.out.println("Je suis majeur.");
         } else {
-            
+            System.out.println("Je ne suis pas encore majeur.");
         }
+        int monAge2 = 30;
+        boolean estMajeur2 = (monAge2 >= 20);
+        System.out.println("Je m'appelle BdR Donatien.");
+        System.out.println("Mon âge est de : " + monAge2 + " ans");
+        if (estMajeur2) {
+            System.out.println("Je suis majeur.");
+        } else {
+            System.out.println("Je ne suis pas encore majeur.");
+        }
+        /*
+         * on peut mettre la condition directement
+         * dans estMajeur, parce que le résultat de la
+         * "fonction" est soit true soit false
+         */
     }
 }
