81e596f8a933acc55b6b5929cb6d0ff017363e55
burindesroziers-julie-emf <julie.dosreis@studentfr.ch>
2024-09-22 09:54:33 +0200

    devoir03 terminé

--- devoirs/devoir03/Devoir03.java
+++ devoirs/devoir03/Devoir03.java
@@ -1,23 +1,51 @@
 package devoir03;
 
+import java.util.Scanner;
+
 public class Devoir03 {
     public static void main(String[] args) {
-        int jour=31;
-        int mois=12;
-        int année=2300;
+        Scanner scanneur = new Scanner(System.in);
+        
+        int année;
+        int mois;
+        int jour;
 
-        boolean bisextile=false;
-        if (année%4==0) {
-            bisextile=true;
-            if (année%400!=0) {
-                bisextile=false;
+        do {
+            System.out.println("Entrer année :");
+            année = scanneur.nextInt();
+        } while (année<0 || année>9999);
+
+        boolean bisextile = false;
+        if (année % 4 == 0) {
+            bisextile = true;
+            if (année % 400 != 0) {
+                bisextile = false;
             }
         }
-        int[] tableauMois = new int[12]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if (bisextile){
-            tableauMois[1]=29;
+
+        do {
+            System.out.println("Entrer mois :");
+            mois = scanneur.nextInt();
+        } while (mois<0 || mois>12);       
+
+        int[] tableauMois = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (bisextile) {
+            tableauMois[1] = 29;
         }
 
-    
+        int maxJourMois = tableauMois[mois-1];
+
+        do {
+            System.out.println("Entrer jour :");
+            jour = scanneur.nextInt();  
+        } while (jour<0 || jour>maxJourMois); 
+
+        int nombreJour = 0;
+        for (int i = 0; i < (mois - 1); i++) {
+            nombreJour += tableauMois[i];
+        }
+
+        nombreJour += jour;
+        System.out.println("Le " + jour + "/" + mois + "/" + année + " est le jour " + nombreJour + " de l'An.");
     }
 }
